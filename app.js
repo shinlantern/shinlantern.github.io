c1e666d0efc16420e2f6d9d58bba671ecabe1a67
Allan Ternola <allan.a.ternola@gmail.com>
2022-07-06 11:32:17 +0800

    Update index

--- app.js
+++ app.js
@@ -5,6 +5,20 @@
                      run.$inject = ['$rootScope'];
                      function run($rootScope) {
                          $rootScope.ProjectTitle = "Lantern Portfolio";
-                         $rootScope.OwnerInformation = "test";
+                         $rootScope.OwnerInfo = {
+                            "FirstName": "Allan",
+                            "LastName": "Ternola",
+                            "Gender":"Male",
+                            "Location":"Baguio City Philippines",
+                            "Email":"allan.a.ternola@gmail.com",
+                            "EducationInfo": [
+                                {"SchoolName":"University of Baguio", "Date":"June 2013 - May 2018", "Course": "Bachelor of Science in Information Technology Major in Software Development"}
+                            ],
+                            "Experiences":[
+                                {"":""}
+                            ]
+                         };
+
+                         console.log($rootScope.OwnerInfo);
                      }
 })();  
